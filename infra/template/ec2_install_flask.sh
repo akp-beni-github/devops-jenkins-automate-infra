7713c6c196ab16478854fe5e1a52c4deecf91e82
Ben <bengzio111@gmail.com>
2024-07-03 15:56:02 +0700

    add runner script

---
+++ infra/template/ec2_install_flask.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+# shellcheck disable=SC2164
+cd /home/ubuntu
+yes | sudo apt update
+yes | sudo apt install python3 python3-pip
+git clone https://github.com/akp-beni-github/flask-python-mysql-db.git
+sleep 20
+# shellcheck disable=SC2164
+cd flask-python-mysql-db
+yes | sudo apt install python3-venv
+python3 -m venv venv
+source venv/bin/activate
+pip install flask
+pip install pymysql
+
+
+yes | sudo apt-get install mysql-client
+mysql -u your_username -p
+CREATE DATABASE devprojdb;
+USE devprojdb;
+CREATE TABLE example_table (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    data TEXT
+);
+exit
+sleep 20
+
+python -m flask --app app.py run --host=0.0.0.0
